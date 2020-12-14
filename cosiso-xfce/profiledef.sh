1eb9ea6f04bfb4a9534a22c96a4518ff463199c3
linlinger <308627244@qq.com>
2020-12-14 16:47:57 +0800

    adjustments

--- cosiso-xfce/profiledef.sh
+++ cosiso-xfce/profiledef.sh
@@ -24,12 +24,5 @@ file_permissions=(
   #Sample to add files
   #[""]=""
   ["/etc/skel"]="0:0:755"
-  ["/etc/skel/.bash_logout"]="0:0:644"
-  ["/etc/skel/.bash_profile"]="0:0:644"
-  ["/etc/skel/.bashrc"]="0:0:644"
-  ["/etc/skel/.xinitrc"]="0:0:644"
-  #["/etc/lightdm"]="0:0:755"
-  #[""]=""
-  #[""]=""
-  #[""]=""
+  ["/etc/skel/.zprofile"]="0:0:644"
 )
